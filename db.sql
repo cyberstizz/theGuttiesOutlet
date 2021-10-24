58c3d6a081e214674bbfd12d3c45a23caaec332a
charleslambdev <charleslambdev@penguin>
2021-10-24 19:06:21 -0400

    completed the products table

--- db.sql
+++ db.sql
@@ -8,4 +8,19 @@ email_address unique not null,
 physical_address varchar(50)
 );
 
-create table products()
+--create the products table with the product id as the primary key
+
+create table products(
+    product_id  integer primary key,
+    product_name varchar(50) unique not null,
+    product_price integer not null,
+    product_description varchar(200) not null,
+    product_quantity integer not null,
+    product_image_one bytea,
+    product_image_two bytea,
+    product_image_three bytea,
+    product_image_four bytea,
+    product_ratings integer,
+    product_delivery_time varchar(20) not null,
+    date_added date not null
+);
