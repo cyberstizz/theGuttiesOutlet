a4c5502d42d3ea4b1f1d9ba0d0f7ba995c67d4fc
charleslambdev <charleslambdev@penguin>
2021-10-24 19:03:17 -0400

    completed user table

--- db.sql
+++ db.sql
@@ -1,3 +1,9 @@
+--first creating the users table with the user id as the primary key
 
-
-create table users();
+create table users(
+user_id varchar(20) primary key,
+session_id integer unique not null,
+user_password varchar(20) not null,
+email_address unique not null,
+physical_address varchar(50)
+);
