1e8ae68f966d62079c224a0160bf31489ac814b6
charleslambdev <charleslambdev@penguin>
2021-10-26 17:23:35 -0400

    created a local config just for testing purposes before deployment on heroku

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-
+    user: "postgres",
+    password: "null",
+    host: "localhost",
+    database: "gutties",
+    port: 5432
 });
+
+
+module.exports = pool;
 
