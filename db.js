f61b2176aaed23c11a93695764d4eb7bef1eee5e
charleslambdev <charleslambdev@penguin>
2021-10-31 04:44:48 -0400

    added turnery logic to select database config

--- db.js
+++ db.js
@@ -9,18 +9,16 @@ const proConfig = {
 };
 
 const localConfig = {
-    
-}
-
-//setting up the config with the correct user, credentials, database and port
-
-const pool = new Pool({
     user: "postgres",
     password: "waterfall",
     host: "localhost",
     database: "postgres",
     port: 5432
-});
+}
+
+//setting up the config with the correct user, credentials, database and port
+
+const pool = new Pool(process.env.build === 'production' ? proConfig : localConfig);
 
 //exporting the variable which represents the configured reference to the --
 //database for use by the server :)
