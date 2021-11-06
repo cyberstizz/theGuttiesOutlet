752f6bd0cb67c8436e053e05cf43fa9ab5fe9382
charleslambdev <charleslambdev@penguin>
2021-11-06 01:21:28 -0400

    set all sections in footer to flex with a direction of column

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,11 +3,29 @@ import './Footer.css';
 
  const Header = () => {
     return(
+
+        // first there is the main footer block which contains all footer elements and is set to a flex direction of row
         <footer id="footerBlock">
-            <section className="footer-logo"></section>
+             {/* now each individual section will be set to flex column */}
+
+             {/* first section will contain the logo and year of conception */}
+
+            <section className="footer-logo">
+
+
+                
+            </section>
+
+
             <section className="footer-left"></section>
+
+
             <section className="footer-center"></section>
+
+
             <section className="footer-right"></section>
+
+
             <section className="footer-social-media"></section>
         </footer>
     )
