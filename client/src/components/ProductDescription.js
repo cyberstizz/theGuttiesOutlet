fef0e262f894517b7c8189dac71371489cf68216
charleslambdev <charleslambdev@penguin>
2021-11-12 17:01:26 -0500

    added beginning of code for the product description page

---
+++ client/src/components/ProductDescription.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+
+const ProductDescription = (props) => {
+
+    return (
+
+        // the first div will be the header block that contains the picture of the item as well as the name and minor details like product id
+        <div className="product_header_block">
+
+            {/* inside there will be two divs representing the left and right side of the header,
+             the left side will the product image, while the right side will contain two sections: set to 
+             flex column with the name and other product info. */}
+
+
+             {/* product image */}
+            <div className="product_image"> </div>
+
+
+            {/* product details */}
+            <div className="product_details">
+
+
+                
+            </div>
+
+        </div>
+    )
+}
