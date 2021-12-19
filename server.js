3a849041995dffee903afdba369e967a952c14d8
charleslambdev <charleslambdev@penguin>
2021-12-19 05:40:23 -0500

    took out path.resolve portion of the code because it was not connecting to the database

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const { encode } = require('punycode');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
-  })
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
+  // })
 }
 // app.use(express.static(path.join(__dirname, '/client')));
  
