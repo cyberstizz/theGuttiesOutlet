41df592296365eddf2cf5038fa90a0a14ac32f9d
charleslambdev <charleslambdev@penguin>
2021-12-22 15:10:08 -0500

    added an if else statement to the server to differentiate between production and local

--- server.js
+++ server.js
@@ -17,11 +17,10 @@ if (process.env.NODE_ENV === 'production') {
   // app.get('*', (req, res) => {
   //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
   // })
-}
-
-
+} else{
 // app.use(express.static(path.join(__dirname, '/client')));
 app.use(express.static(path.join(__dirname, '/client')));
+}
 // enabling cors
 app.use(cors());
  
