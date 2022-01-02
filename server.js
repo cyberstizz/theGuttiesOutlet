1e6418090f71fca27e2c15573e3cdce91450f919
charleslambdev <charleslambdev@penguin>
2022-01-02 05:43:18 -0500

    condensed server down to 26 lines

--- server.js
+++ server.js
@@ -1,63 +1,24 @@
-//imrrrporting important variables
- 
+//importing important variables
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
 const path = require('path');
-const pool = require('./db');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-
-
 // importing all of the routes as variables known as routers
-
 const productsRouter = require('./routes/products/productsRouter');
 const homeRouter = require('./routes/home/homeRouter');
 const testRouter = require('./routes/test/testRouter');
-
- 
 // initializing the entry point of the index.html from the build folder if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname,'client/build')))
-
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
-  // })
-}
-
- 
+} 
 // sending all routes to its appropriate express router
-// app.use('/products', productRouter);
- 
-
-// app.use(fileUpload())
  
 // initiate use of those router variables through middleware (or app.get)
-
- 
 app.use('/home', homeRouter);
-
-//  app.use(homeRouter);
-
-
 
 app.use('/products', productsRouter);
  
- 
-
-
- app.use('/test',testRouter);
-
-
-
-
- 
-  
-
-
-
-
-
+app.use('/test',testRouter);
 
  
  
