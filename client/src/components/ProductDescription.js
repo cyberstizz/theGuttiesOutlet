0a0ee70a3767b48f2f08384f14e4a84dd8256023
charleslambdev <charleslambdev@penguin>
2022-01-06 19:45:54 -0500

    changed the hover stste of headers search br button

--- client/src/components/ProductDescription.js
+++ client/src/components/ProductDescription.js
@@ -34,28 +34,37 @@ const ProductDescription = () => {
 
     useEffect(() => {
 
-
+        // this function occurs every time a change takes place on the dom
+       
         const callTheDatabase = async () => {
-
-            // const response = await axios.get('/home');
-
-            // const responseComplete = await response.data;
-        
-            // console.log(responseComplete)
-            // const { theName, thePrice, theDescription, theSneakerPath } = responseComplete;
             
+            // inside of this function their is a call to the database for the product details
+
             const firstCall = await axios.get(`/products/${productId}`)
+
+            // after the call to the datase this next variable is the resolved promise that we will
+            // used to destructure the data we need individually
+
             const firstCallResponse = await firstCall.data;
+
+            // this is where we destructure for the name price description and path
 
             const { theName, thePrice, theDescription, theSneakerPath } = firstCallResponse;
 
-            console.log(theName)
+            // and below we set these destructured variables to the useState variables to pass into
+            // our preferred components
+
             setName(theName)
             setSneakerPath(theSneakerPath);
             setPrice(thePrice);
             setDescription(theDescription);
             setSneakerPath(theSneakerPath);
+
+            // now this is where the function CallTheDatabase ends meaning it has not been called
         }
+
+        // finally the function is called below becaause this function is only DEFINED in the useEffect
+        // but we still have to call the function every time useEffect is called, so here it is...
 
         callTheDatabase()
     })
@@ -71,11 +80,15 @@ const ProductDescription = () => {
         <section className="topBlock">
             {/*this is the picture*/}
             <img className="productPic" src={SneakerPath} />
-            
+            {/* this is the entire product purchase sections */}
             <section className="productPurchaseSection">
+            {/* this block represents the left side with the buttons and details */}
             <div className="rightSectionBlock">
+                {/* this section is for the title of the product */}
                 <div className="purchaseSections" id="headline" style={{color: '#B48B22'}}>{Name}</div>
+                {/* this section is for the price */}
                 <div className="purchaseSections" id="price"><div style={{color: 'white', marginLeft: '5vw', "font-family": "'Permanent Marker', cursive"}}>Price:</div> <span style={{marginLeft: '1vw', "font-family": "'Permanent Marker', cursive"}}>${Price}</span></div>
+                {/* this is the quantity section where the user can select their quantity via a dropdown */}
                 <div className="purchaseSections" style={{marginLeft: '9vw', borderBottom: 'solid 3px #B48B22', width: '25vw', paddingBottom: '4px'}}> Quantity:
                       <select onChange={selectChangeHandler} style={{marginLeft: '1vw',backgroundColor:'#0D2A57', color: '#B48B22', "font-family": "'Permanent Marker', cursive", height: '30px', width: '35px', fontSize: '26px', border:'solid 1px #0D2A57'}}>
                         <option>1</option>
@@ -86,9 +99,11 @@ const ProductDescription = () => {
 
                       </select>
                 </div>
+                {/* this section diplays the price */}
                 <div className="purchaseSections" id="price"><div style={{color: 'green', marginLeft: '5vw', "font-family": "'Permanent Marker', cursive"}}><span style={{color: 'white', marginLeft: '-1.2vw'}}>Total:  </span>${!quantity ? Price : (Price * quantity)}</div> <span style={{marginTop: '1vh', marginLeft: '1vw'}}></span></div>
-
+                {/* this div is for the estimated delivery */}
                 <div className="purchaseSections">Est. Delivery: <span id="date">January 9th, 2022</span></div>
+                 {/* and the two buttons are below */}
                 <div className="buttonsDiv"><Link to={`cart/${productId}`}><button id="purchaseButtonCart">Add to cart</button></Link>  <button id="purchaseButtonBuy">Buy now</button> </div>
 
             </div>
