26e96da55253b0d43b2d00b4452100917400277e
charleslambdev <charleslambdev@penguin>
2022-01-11 11:06:07 -0500

    added most of payment route

--- routes/payments/paymentsRouter.js
+++ routes/payments/paymentsRouter.js
@@ -27,6 +27,49 @@ paymentsRouter.use(express.json());
 
 paymentsRouter.post('/', async (req, res, next) => {
         const {product, token } = req.body;
+
+        const idompotencyKey = uuid();
+
+        const customer = await stripe.customers.create({
+
+        })
+
+        const charge = await stripe.charges.create({
+
+        }, {idompotencyKey})
+
+        console.log(charge)
+
+
+
+        let transport = nodemailer.createTransport({
+            host: 'www.charles.lamb.dev@gmail.com',
+            port: 2525,
+            auth: {
+               user: 'put_your_username_here',
+               pass: 'put_your_password_here'
+            }
+        });
+
+
+
+        const message = {
+            from: 'charles.lamb.dev@gmail.com', // Sender address
+            to: 'charles.lamb.dev@gmail.com',         // recipient
+            subject: 'just testing emails from ther server', // Subject 
+            text: 'everything seems to be working!' // message
+        };
+
+
+        transport.sendMail(message, function(err, info) {
+            if (err) {
+              console.log(err)
+            } else {
+              console.log(info);
+            }
+        });
+
+        res.status(200).json(charge)
    });
     
    
