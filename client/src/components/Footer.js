a50803dfed22ce8184fba7e23d30f096f949f3e8
charleslambdev <charleslambdev@penguin>
2022-03-31 15:24:57 -0400

    added mobile version of footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
  const Footer = () => {
     return(
 
-        // first there is the main footer block which contains all footer elements and is set to a flex direction of row
+        <React.Fragment>
+
         <footer id="footerBlock">
              {/* now each individual section will be set to flex column */}
 
@@ -63,6 +64,67 @@ import { Link } from "react-router-dom";
                 
             </section>
         </footer>
+
+
+        {/* // now the mobile section */}
+
+        <footer id="mobilefooterBlock">
+             {/* now each individual section will be set to flex column */}
+
+             {/* first section will contain the logo and year of conception */}
+
+            <section className="mobilefooter-logo-section">
+
+
+            <div className="mobilefooter-logo"> </div>
+
+            <div className="mobilefooter-conception-year"> @2021 </div>
+
+            </section>
+
+
+            <section className="mobilefooter-left">
+
+                <ul className="mobilefooter-list-block">
+                    <li className="mobilefooter-section-header">sitemap</li>
+                  <Link to='/'><li className="mobilefooter-list-item">Home</li></Link>
+                  <Link to='/newArrivals'><li className="mobilefooter-list-item">New Arrivals</li></Link>
+                  <Link to='/bestSellers'><li className="mobilefooter-list-item">Best Sellers</li></Link>
+                  <Link to='/discounts'><li className="mobilefooter-list-item">Discount</li></Link>
+
+                </ul>
+            </section>
+
+
+            <section className="mobilefooter-center">
+            <ul className="mobilefooter-list-block">
+                    <li className="mobilefooter-section-header">Company</li>
+                    <li className="mobilefooter-list-item">About Us</li>
+                    <li className="mobilefooter-list-item">Contact</li>
+                </ul>
+
+
+            </section>
+
+
+            <section className="mobilefooter-right">
+
+            <ul className="mobilefooter-list-block">
+                    <li className="mobilefooter-section-header">Policies</li>
+                    <li className="mobilefooter-list-item">Privacy Policy</li>
+                    <li className="mobilefooter-list-item">Cookie Policy</li>
+                    <li className="mobilefooter-list-item">Terms Of Use</li>
+                </ul>
+
+            </section>
+
+
+            <section className="mobilefooter-social-media">
+                    <div className="mobilesocial-media-buttons-block"></div>
+                
+            </section>
+        </footer>
+        </React.Fragment>
     )
 };
 
